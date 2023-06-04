137fb765bd9e2e07b474389afb8835262c630e72
Tomas Urroz <73548701+TomasUrroz@users.noreply.github.com>
2023-06-04 16:00:10 -0300

    Commit inicial con lo sacado de Drop

---
+++ desktop/src/com/game/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.game;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("Game");
+		config.setWindowedMode(800, 480);
+		config.useVsync(true);
+		config.setForegroundFPS(60);
+		new Lwjgl3Application(new Drop(), config);
+	}
+}
